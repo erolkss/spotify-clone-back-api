2b7c5e150345bc4a09df27c38aee48e08a994b69
Lucas Lima <lucaserolima@gmail.com>
2025-07-07 19:47:09 -0400

    Create project back to Spotify Clone

---
+++ src/main/java/br/com/erolkss/spotify_clone_back/SpotifyCloneBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.erolkss.spotify_clone_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erolkss/spotify_clone_back/SpotifyCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erolkss.spotify_clone_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
